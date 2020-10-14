e6c1f5558e13e1271c60522a6200e84defbd959a
RagingZonkey <yegorkauser228@outlook.com>
2020-10-14 20:10:05 +0300

    1 new constructor + all the variables created + output method done

--- Лабораторная3_ООП/Program.cs
+++ Лабораторная3_ООП/Program.cs
@@ -11,23 +11,58 @@ namespace Лабораторная3_ООП
 
     partial class Book
     {
-        int Id;
         public string Name;
         public string Authors;
         public string Publishing_House_Name;
         int Pages_Amount;
-        int Price;
+        float Price;
         public string Binding_Type;
-        static readonly string R_O = "This is a read-only message";
+        static readonly string Id;
+        static int Object_Counter = 0;
 
 
         public Book()
         {
+            Name = "Преступление и наказание";
+            Authors = "Ф.М. Достоевский";
+            Publishing_House_Name = "Азбука";
+            Pages_Amount = 608;
+            Price = 8.50;
+            Binding_Type = @"""твердый""";
+            Id = Name.GetHashCode() + Authors.GetHashCode();
+            Object_Counter++;
+        }
+
+        static Book()
+        {
+            Console.WriteLine("You are working with a static constructor");
 
         }
 
 
     }
+
+    partial class Book()
+        {
+
+          public void Output()
+        {
+            Console.WriteLine($"Книга под названием {this.Name}" +
+                $" автора {this.Authors}" +
+                $" издательства {this.Publishing_House_Name}" +
+                $" в переплете типа {this.Binding_Type}" +
+                $" толщиной {this.Pages_Amount} страниц, " +
+                $"имеет уникальный номер {this.PlacesLuxury} " +
+                $"и стоит {this.PlacesReserved} рублей");
+        }
+
+        static void Class_Info()
+        {
+            Console.WriteLine($"Количество созданных экземпляров объекта Book равно: {Objects_Counter}");
+        }
+      
+
+        }
     class Program
     {
         static void Main(string[] args)
